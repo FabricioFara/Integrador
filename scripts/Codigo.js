46e7cd71f10545624d7894295e1babae6cc17b8f
facuulp <163307592+facuulp@users.noreply.github.com>
2024-05-27 12:52:40 -0300

    Modificando carrousel clicks

---
+++ scripts/Codigo.js
@@ -0,0 +1,9 @@
+const imagenes= ['imagenes carrousel/ima1','imagenes carrousel/ima2','imagenes carrousel/ima3','imagenes carrousel/ima4','imagenes carrousel/ima5','imagenes carrousel/ima6']
+function izquierda() {
+    if (flechaiz==-1) {
+        
+
+
+        
+    }
+}
