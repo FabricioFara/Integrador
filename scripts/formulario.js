93032ffd7c1055dfec3f38c398a4eeb80b723ff9
facuulp <163307592+facuulp@users.noreply.github.com>
2024-05-27 19:42:12 -0300

    Casi terminado seccion formulario

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -1,5 +1,38 @@
 let textnombre = document.getElementById("iname");
 let textapellido = document.getElementById("iapell");
-let correo = document.getElementById("iemail");
+let numero = document.getElementById("inum");
 let error = document.getElementById("mostrar-error");
 let datos = document.getElementById("mostrar-dato");
+function validar() {
+    error.innerHTML=" ";
+    textnombre.classList.remove("error")
+    let errores=[];
+    let tel = /^[1-9]\d{9}$/;
+    let nbr=textnombre.value.trim();
+    let apell=textapellido.value.trim();
+    if(nbr.lenght==0) {
+        errores.push("falta el nombre");
+        textnombre.classList.add("error");
+
+    } else if(nbr.lenght>9) {
+        errores.push("nombre muy largo");
+        textnombre.classList.add("error");
+    }
+    if(apell.lenght==0) {
+        errores.push("falta el apellido");
+        textapellido.classList.add("error");
+
+    } else if(apell.lenght>9) {
+        errores.push("apellido muy largo");
+        textapellido.classList.add("error");
+    }
+if(numero.value.lenght>10) {
+    errores.push("Numero muy largo");
+    numero.classList.add("error");
+
+}
+if(numero.value.lenght>0 && !tel.test(numero.value) ) {
+    errores.push("Numero invalido(no puede comenzar con 0 y no puede ser mayor que 10)");
+    numero.classList.add("error");
+}
+}
