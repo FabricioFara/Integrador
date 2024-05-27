2d48a3f3c7710012ac65cc18741ccc2a30fccf4c
facuulp <163307592+facuulp@users.noreply.github.com>
2024-05-27 17:13:34 -0300

    Terminando carrousel de imagenes

--- scripts/Codigo.js
+++ scripts/Codigo.js
@@ -1,9 +1,37 @@
-const imagenes= ['imagenes carrousel/ima1','imagenes carrousel/ima2','imagenes carrousel/ima3','imagenes carrousel/ima4','imagenes carrousel/ima5','imagenes carrousel/ima6']
-function izquierda() {
-    if (flechaiz==-1) {
-        
+const imagenes = [
+    'imagenes carrousel/ima1.jpg', 
+    'imagenes carrousel/ima2.jpg', 
+    'imagenes carrousel/ima3.jpg', 
+    'imagenes carrousel/ima4.jpg', 
+    'imagenes carrousel/ima5.jpg', 
+    'imagenes carrousel/ima6.jpg'
+];
 
+let indiceActual = 0;
 
-        
-    }
+function mostrarImagen(indice) {
+    const imgElement = document.getElementById('inicio');
+    imgElement.src = imagenes[indice];
 }
+
+function izquierda() {
+    if (indiceActual > 0) {
+        indiceActual--;
+    } else {
+        indiceActual = imagenes.length - 1;
+    }
+    mostrarImagen(indiceActual);
+}
+
+function derecha() {
+    if (indiceActual < imagenes.length - 1) {
+        indiceActual++;
+    } else {
+        indiceActual = 0;
+    }
+    mostrarImagen(indiceActual);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarImagen(indiceActual);
+});
