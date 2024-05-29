c84b5dce0845475ff9f35ba1a24562a2f10e6e62
FabricioFara <farafabri1994@gmail.com>
2024-05-29 20:02:15 -0300

    Correccion definitiva de formulario
    se logro completar el formulario de manera exitosa..!!

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -6,40 +6,59 @@ let datos = document.getElementById("mostrar-dato");
 function validar() {
     error.innerHTML=" ";
     datos.innerHTML=" ";
-    textnombre.classList.remove("error")
+    textnombre.classList.remove("error");
+    textapellido.classList.remove("error");
+    numero.classList.remove("error");
     let errores=[];
     let tel = /^[1-9]\d{9}$/;
+    let nombreRegex = /^[A-Za-z\s]+$/;
     let nbr=textnombre.value.trim();
     let apell=textapellido.value.trim();
+    let num = numero.value.trim();
     
     if(nbr.length==0) {
         errores.push("falta el nombre");
         textnombre.classList.add("error");
+        textnombre.value = "";
 
-    } else if(nbr.length>9) {
+    } else if(nbr.length>15) {
         errores.push("nombre muy largo");
         textnombre.classList.add("error");
+        textnombre.value = "";
+
+    }else if (!nombreRegex.test(nbr)) {
+        errores.push("El nombre no puede contener números ni caracteres especiales");
+        textnombre.classList.add("error");
+        textnombre.value = "";
     }
     
     if(apell.length==0) {
         errores.push("falta el apellido");
         textapellido.classList.add("error");
+        textapellido.value = "";
 
     } else if(apell.length>9) {
         errores.push("apellido muy largo");
         textapellido.classList.add("error");
+        textapellido.value = "";
+    }else if (!nombreRegex.test(apell)) {
+        errores.push("El apellido no puede contener números ni caracteres especiales");
+        textapellido.classList.add("error");
+        textapellido.value = "";
     }
-    
-if(numero.value.length>10) {
-    errores.push("Numero muy largo");
-    numero.classList.add("error");
 
-}
-if(numero.value.length<0 && !tel.test(numero.value) ) {
-    errores.push("Numero invalido(no puede comenzar con 0 y no puede ser mayor que 10)");
+    if(num.length==0){
+    errores.push("falta el telefono");
     numero.classList.add("error");
+    numero.value = "";
     
-}
+    }else if (!tel.test(num)) {
+        errores.push("Número inválido (No debe contener caracteres, no comenzar con 0 y debe tener 10 digitos)");
+        numero.classList.add("error");
+        numero.value = "";
+    }
+
+
 for(let err of errores){
     let li = document.createElement("li");
     error.appendChild(li);
